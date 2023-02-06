a2764b3a28944c43ab7f07d687c690a97dca89cc
鍾政廣 <paul930237@gmail.com>
2023-02-06 18:26:20 +0800

    由於stripe 在台灣不給用，

---
+++ lib/stripe.ts
@@ -0,0 +1,26 @@
+import {
+  createCheckoutSession,
+  getStripePayments,
+} from "@stripe/firestore-stripe-payments";
+import { getFunctions, httpsCallable } from "@firebase/functions";
+import app from "../firebase";
+
+const payments = getStripePayments(app, {
+  productsCollection: "products",
+  customersCollection: "customers",
+});
+
+const loadCheckout = async (priceId: string) => {
+  await createCheckoutSession(payments, {
+    price: priceId,
+    success_url: window.location.origin,
+    cancel_url: window.location.origin,
+    // 可制定失敗cancel_url頁面 ＥＸ.
+    // cancel_url:https://localhost.3000
+  })
+    .then((snapshot) => window.location.assign(snapshot.url))
+    .catch((error) => console.log(error.message));
+};
+
+export { loadCheckout };
+export default payments;
--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withTM = require("next-transpile-modules")([
+  "@stripe/firestore-stripe-payments",
+]);
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["image.tmdb.org"],
+//     // 導入網址圖片用
+//   },
+// };
+
+module.exports = withTM({
   reactStrictMode: true,
   images: {
-    domains: ["image.tmdb.org"],
+    domains: ["image.tmdb.org", "rb.gy"],
     // 導入網址圖片用
   },
-};
-
-module.exports = nextConfig;
+});
